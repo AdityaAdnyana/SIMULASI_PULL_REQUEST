470d5e1385f138a01410f3501f638d2c1185cbf8
Farid-lol <farid.j.o.g@gmail.com>
2026-03-09 09:43:54 +0700

    menambahkan Modul 2

---
+++ MODUL2_103082400018/Pendahuluan/arrayGenap/arrayGenap/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arrayGenap = {2,4,6,8,10 };
+        int number = 1;
+
+        foreach(int angka in arrayGenap)
+        {
+            Console.WriteLine($"Angka genap ke {number} : {angka}");
+            number++;
+        }
+        
+    }
+}
