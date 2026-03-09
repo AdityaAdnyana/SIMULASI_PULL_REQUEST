47e055636fbc97628c5be8475fe395434d56c83f
Farid-lol <farid.j.o.g@gmail.com>
2026-03-09 10:26:09 +0700

    cimmit 1

---
+++ InputNama/InputNama/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InputNama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan nama Anda: ");
+            string nama = Console.ReadLine();
+            Console.WriteLine($"Sekamat datang , {nama}!");
+        }
+    }
+}
---
+++ inputAngka/inputAngka/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("masukan angka 1 - 1000 :");
+        int angka = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"angka  : {angka}");
+
+    }
+}
