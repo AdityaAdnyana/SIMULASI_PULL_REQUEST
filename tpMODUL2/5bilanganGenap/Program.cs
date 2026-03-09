11a17e589a21d971a42d9a782c807711510078c8
Ridho-aw <r9480.a2b@gmail.com>
2026-03-09 08:34:29 +0700

    cimmit 1

---
+++ tpMODUL2/5bilanganGenap/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        for (int i = 1; i < 6; i++)
+        {
+            int angka = 2;
+            angka = angka * i;
+            Console.WriteLine($"Angka genap {i} : {angka}");
+        }
+    }
+}
---
+++ tpMODUL2/tpMODUL2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Masukan huruf : ");
+        string input = Console.ReadLine();
+        
+        switch (input.ToUpper())
+        {
+            case "A":
+                Console.WriteLine($"Huruf {input} merupakan huruf vokal");
+                break;
+            case "I":
+                Console.WriteLine($"Huruf {input} merupakan huruf vokal");
+                break;
+            case "U":
+                Console.WriteLine($"Huruf {input} merupakan huruf vokal");
+                break;
+            case "E":
+                Console.WriteLine($"Huruf {input} merupakan huruf vokal");
+                break;
+            case "O":
+                Console.WriteLine($"Huruf {input} merupakan huruf vokal");
+                break;
+            default:
+                Console.WriteLine($"Huruf {input} merupakan huruf konsonan");
+                break;
+        }
+
+    }
+}
