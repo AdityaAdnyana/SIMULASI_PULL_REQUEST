7ec9a1539499b92e5911f752ad6f52f162084775
Ridho-aw <r9480.a2b@gmail.com>
2026-03-09 10:08:20 +0700

    Merge unrelated histories

---
+++ Code/Code/bilanganPrima/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Masukkan bilangan :");
+        int bilangan = Convert.ToInt32(Console.ReadLine());
+        if (bilPrima(bilangan))
+        {
+            Console.WriteLine($"{bilangan} adalah bilangan prima");
+        }
+        else
+        {
+            Console.WriteLine($"{bilangan} Bukan bilangan prima");
+        }
+    }
+
+    public static bool bilPrima(int n)
+    {
+        if (n <= 1)
+        {
+            return false;
+        }
+        if (n <= 3)
+        {
+            return true;
+        }
+        if (n % 2 == 0 || n % 3 == 0)
+        {
+            return false;
+        }
+        for (int i = 5; i * i <= n; i += 6)
+        {
+            if (n % i == 0 || n % (i + 2) == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
